5eba9d7f1d1b2b57644c88df16b5b40d4f99f20b
akash <akash619@protonmail.com>
2019-06-11 14:15:15 +0530

    Added DOC Login Form

--- client/src/DocRegister.js
+++ client/src/DocRegister.js
@@ -8,7 +8,7 @@ class DocRegister extends Component {
                 <main class="pa4 black-80">
                     <form class="measure center">
                         <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-                            <legend class="f4 fw6 ph0 mh0">Sign In</legend>
+                            <legend class="f4 fw6 ph0 mh0">Log In</legend>
                             <div class="mt3">
                                 <label class="db fw6 lh-copy f6" for="email-address">Email</label>
                                 <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
